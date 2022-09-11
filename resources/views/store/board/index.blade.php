0bde8a62b2472c4baebf885f35fe461ef337d670
Leojfl <42425825+Leojfl@users.noreply.github.com>
2022-09-10 19:35:57 -0500

    Tables index

--- resources/views/store/board/index.blade.php
+++ resources/views/store/board/index.blade.php
@@ -17,68 +17,60 @@
             <a href="{{ route('store_board_map') }}"
                class="btn btn-secondary btn-upsert">Mapa</a>
         </div>
-        <div class="input-group mb-3">
+        <div class="col-12">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Descripción</th>
+                    <th scope="col">Capacidad</th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
                 @forelse($boards as $board)
-                <div class="modal-body">
-
-                    <div class="p-2 bg-ligth border" style="border-radius: 45px">
-
-                        <h5>ID: {{$board->id}}</h5>
-
-                        <h1>{{$board->name}}</h1>
-                    </div>
-
-                        <br>
-                        <div class="p-2 bg-ligth border" style="border-radius: 45px">
-                        <h2>Descripcion :{{$board->description}}</h2>
-                        <br>
-                        <h2>Activo :{{$board->active}}</h2>
-                        <br>
-                        <h2>Disponible :{{$board->available}}</h2>
-                        <br>
-
-                        <pre>               </pre>
-
-
+                    <tr>
+                        <td>{{$board->id}}</td>
+                        <td>{{$board->name}}</td>
+                        <td>{{$board->description}}</td>
+                        <td>{{$board->capacity}}</td>
+                        <td>
                             <a href="{{ route('store_board_upsert', $board->id) }}"
-
-                               data-toggle="tooltip"
-                               data-placement="top"
-                               title="Editar"
-                               class="btn-upsert">
-                                <i class="fas fa-edit "></i>
-                            </a>
-                            <a href="{{ route('store_board_profile', $board->id) }}"
-
-                               data-toggle="tooltip"
-                               class="btn-upsert"
-                               title="Ver">
-                                <i class="far fa-eye "></i>
-                            </a>
-                            <a href="{{ route('store_board_delete', $board->id) }}"
-
+                                data-toggle="tooltip"
+                                data-placement="top"
+                                title="Editar"
+                                class="btn-upsert">
+                                 <i class="fas fa-edit "></i>
+                             </a>
+                             <a href="{{ route('store_board_profile', $board->id) }}"
                                 data-toggle="tooltip"
                                 class="btn-upsert"
                                 title="Ver">
-                                 <i class="far fa-trash-alt "></i>
+                                 <i class="far fa-eye "></i>
+                             </a>
+                             <a href="{{ route('store_board_delete', $board->id) }}"
+                                 data-toggle="tooltip"
+                                 class="btn-upsert"
+                                 title="Ver">
+                                  <i class="far fa-trash-alt "></i>
+                              </a>
+                             <a class="btn-update-status">
+                                 <i class="fas {{$board->active?'fa-toggle-on':'fa-toggle-off'}}"></i>
                              </a>
 
-                            <a
-                               class="btn-update-status">
-                                <i class="fas {{$board->active?'fa-toggle-on':'fa-toggle-off'}}"></i>
-                            </a>
-
-                        </div>
-
-                </div>
+                        </td>
+                    </tr>
                 @empty
                     <tr>
                         <th class="text-center" colspan="6">
                             <i>Sin mesas registradas</i>
                         </th>
                     </tr>
+
                 @endforelse
-            </div>
+                </tbody>
+            </table>
         </div>
     </div>
     @include('commons.modal',['modalId'=> 'modal-upsert'])
